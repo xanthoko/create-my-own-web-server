f3afa0d7c862efaf428e2ee749853bcb384e944a
xantho <konxantho@gmail.com>
2018-10-01 00:58:52 +0300

    simple server implementation

---
+++ webserver3a.py
@@ -0,0 +1,32 @@
+import socket
+
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
+REQUEST_SIZE_QUEUE_SIZE = 5
+
+
+def handle_request(client_connection):
+    request = client_connection.recv(1024)
+    print(request.decode())
+    http_response = b"""\
+HTTP/1.1 200 OK
+
+Hello world!
+"""
+    client_connection.sendall(http_response)
+
+
+def server_forever():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(SERVER_ADDRESS)
+    listen_socket.listen(REQUEST_SIZE_QUEUE_SIZE)
+    print('Serving HTTP on port {} ...'.format(PORT))
+
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        handle_request(client_connection)
+        client_connection.close()
+
+
+if __name__ == '__main__':
+    server_forever()
